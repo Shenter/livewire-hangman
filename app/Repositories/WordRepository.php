f1989ba8826c44a6516b5ab2e42d1b0991c0f61a
shenter <shenter@yandex.ru>
2021-09-06 08:36:28 +0300

    Updated Readme

--- app/Repositories/WordRepository.php
+++ app/Repositories/WordRepository.php
@@ -10,6 +10,7 @@ class WordRepository
     public function generateWord():string
     {
         $array = ['ЯБЛОКО','МОРОЗ','ЗМЕЯ','РУЛЬ','ПУЛЬТ','СМЕЛОСТЬ','ЗРЕЛОСТЬ','УСПЕХ','ДОСПЕХ','ВИШНЯ','ДОЖДЬ'];
+//        $array = ['ЯБЛОКО','МОРОЗ'];
         return $array[array_rand($array)];
     }
 }
